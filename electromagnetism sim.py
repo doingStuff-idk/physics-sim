f3253b48610fe00835765cdbdac6f4bdc0d0bb9a
Noam HaIM <noamhaim6@gmail.com>
2026-04-28 23:02:49 +0300

    compiles, no field->particle interaction yet, field diverges idk why

--- electromagnetism sim.py
+++ electromagnetism sim.py
@@ -55,22 +55,13 @@ for l in range(1,steps):
         for y in range(ny):
             for z in range(nz):
                 #now we will look at the derivatives, which here are forward (deriv at x,y,z is diff between x+1 and x), this sucks
-                derEX,derEY,derEZ,derBX,derBY,derBZ = 0,0,0,0,0,0
-                if x + 1 >= nx:
-                    derEX=0
-                    derBX=0
+                derEX,derEY,derEZ,derBX,derBY,derBZ = np.zeros(3),np.zeros(3),np.zeros(3),np.zeros(3),np.zeros(3),np.zeros(3)
                 if x+1<nx:
                     derEX = (E[x + 1, y, z] - E[x, y, z]) / dx
                     derBX = (B[x + 1, y, z] - B[x, y, z]) / dx
-                if y + 1 >= ny:
-                    derEY=0
-                    derBY=0
                 if y + 1 < ny:
                     derEY = (E[x, y + 1, z] - E[x, y, z]) / dy
                     derBY = (B[x, y + 1, z] - B[x, y, z]) / dy
-                if z + 1 >= nz:
-                    derEZ=0
-                    derBZ=0
                 if z+1<nz:
                     derEZ = (E[x, y, z + 1] - E[x, y, z]) / dz
                     derBZ = (B[x, y, z + 1] - B[x, y, z]) / dz
@@ -78,27 +69,28 @@ for l in range(1,steps):
                 if derEX.any() or derEY.any() or derEZ.any():
                     correcB.append([[x,y,z],[derEZ[1]-derEY[2],derEX[2]-derEZ[0],derEY[0]-derEX[1]]])
                     sizecorB = sizecorB + 1
-                    J = 0
+                    j = [0,0,0]
                     for i in particles:
                         tempx = np.floor(i.position[0] / dx)
                         tempy = np.floor(i.position[1] / dy)
                         tempz = np.floor(i.position[2] / dz)
                         if ([tempx,tempy,tempz] == [x,y,z]):
                             dens = i.charge/dv
-                            J = [i.velocity[0]*dens,i.velocity[1]*dens,i.velocity[2]*dens]
-                    if J!=0 or derBX!=0 or derBY!=0 or derBZ!=0:
+                            j = [i.velocity[0]*dens,i.velocity[1]*dens,i.velocity[2]*dens]
+                    if j!=0 or derBX!=0 or derBY!=0 or derBZ!=0:
                         tempV = [(derBY[2]-derBZ[1])/(mu*eps)-j[0]/eps,(derBZ[0]-derBX[2])/(mu*eps)-j[1]/eps,(derBX[1]-derBY[0])/(mu*eps)-j[2]/eps]
                         correcE.append([[x,y,z],tempV])
                         sizecorE = sizecorE + 1
     #i have found all the corrections needed to be done to the fields. now we need to use them to update our fields
     #NEED LENGTH OF CORRECTION MATRICES, GO THROGUH THEM CHANGE ELEMENT ELEMENT, DONT TRY TO BE SMART AND EFFICIENT, YOU AINT TURIN
-    for i in sizecorE:
+    for i in range(sizecorE):
         x,y,z = correcE[i][0][0],correcE[i][0][1],correcE[i][0][2]
         arr = np.array([correcE[i][1][0],correcE[i][1][1],correcE[i][1][2]])
         E[x,y,z] = E[x,y,z] + arr*dt
-    for i in sizecorB:
+    for i in range(sizecorB):
         x,y,z = correcB[i][0][0],correcB[i][0][1],correcB[i][0][2]
         arr = np.array([correcB[i][1][0],correcB[i][1][1],correcB[i][1][2]])
+        print(B[x,y,z])
         B[x,y,z] = B[x,y,z] + arr*dt
     #DONE WITH UPDATING FIELDS, NOW NEED TO CALCULATE FORCE ACTING ON EACH PARTICLE, I.E RETRIEVE THE FIELD AT THE LOC OF THE PARTILCE. MAYBE BORIS ALGO?
 
